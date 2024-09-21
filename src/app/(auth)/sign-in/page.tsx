4553acfc2baa29ef8e993257e58e2c68986274ab
Mohammed Yunus Shaikh <shaikhyunus.us89@gmail.com>
2024-09-21 22:16:41 +0530

    Added products and media to payload

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -13,40 +13,57 @@ import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/valid
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
 import { ZodError } from "zod";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Page = () => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const isSeller = searchParams.get('as') === 'seller'
+    const origin = searchParams.get('origin')
+
+    const continueAsSeller = () => {
+        router.push(`?as=seller`)
+    }
+
+    const continueAsBuyer = () => {
+        router.replace('/sign-in', undefined)
+    }
 
     const { register, handleSubmit, formState: {errors} } = useForm<TAuthCredentialsValidator>({
         resolver: zodResolver(AuthCredentialsValidator),
     })
 
-    const router = useRouter();
+    
 
-    const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-        onError: (err) => {
-            if (err.data?.code === "CONFLICT") {
-                toast.error("This email is already in use. Sign in instead")
-                return
-            }
+    const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
+        onSuccess: () => {
+            toast.success("Signed in successfully")
             
-            if(err instanceof ZodError) {
-                toast.error(err.issues[0].message)
+            router.refresh()
+
+            if(origin) {
+                router.push(`/${origin}`)
+                return   
+            }
+
+            if(isSeller) {
+                router.push('/sell')
                 return
             }
 
-            toast.error('Something went wrong. Please try again.')
+            router.push('/')
         },
-        onSuccess: ({sentToEmail}) => {
-            toast.success(`Verification email sent to ${sentToEmail}.`)
-            router.push('/verify-email?to= ' + sentToEmail)
+        onError: (err) => {
+            if(err.data?.code === "UNAUTHORIZED") {
+                toast.error("Invalid email or password.")
+            }
         }
     })
 
     const onSubmit = ({ 
         email, password 
     }: TAuthCredentialsValidator) => {
-        mutate({ email, password })
+        signIn({ email, password })
     }
 
     return (
@@ -56,7 +73,7 @@ const Page = () => {
                         <div className='flex flex-col items-center space-y-2 text-center'>
                             <Icons.logo className='h-20 w-20' />
                              <h1 className='text-2xl font-semibold tracking-tight'>
-                                Create an account
+                                Sign in to your {isSeller ? "seller" : ""} account  
                             </h1>
 
                             <Link 
@@ -64,8 +81,8 @@ const Page = () => {
                                 variant: 'link',
                                 className: "gap-1.5"
                             })} 
-                            href="/sign-in">
-                                Already have an account? Sign-in
+                            href="/sign-up">
+                                Don&apos;t have an account
                                 <ArrowRight className="h-4 w-4" />
                             </Link>
                         </div>
@@ -109,9 +126,37 @@ const Page = () => {
                                         )}
                                     </div>
 
-                                    <Button>Sign up</Button>
+                                    <Button>Sign in</Button>
                                 </div>
                             </form>
+
+                            <div className="relative">
+                                <div 
+                                    aria-hidden="true"
+                                    className="absolute inset-0 flex items-center">
+                                        <span className="w-full border-t" />
+                                </div>
+                                <div className="relative flex justify-center text-xs uppercase">
+                                    <span className="bg-background px-2 text-muted-foreground">or</span>
+                                </div>
+                            </div>
+
+                            {isSeller ? (
+                                <Button
+                                variant="secondary"
+                                onClick={continueAsBuyer}
+                                disabled={isLoading}
+                                >
+                                    Continue as customer
+                                </Button>
+                            ) : (
+                                <Button
+                                variant="secondary"
+                                onClick={continueAsSeller}
+                                >
+                                    Continue as seller
+                                </Button>
+                            )}
                         </div>
                     </div>
                 </div>
--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,9 +2,11 @@ import { AuthCredentialsValidator } from "../lib/validators/account-credentials-
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import payload from "payload";
 
 export const authRouter = router({
-    createPayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation( async ({input}) => {
+    createPayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation( async ({ input }) => {
         const { email, password } = input
         const payload = await getPayloadClient()
 
@@ -14,7 +16,7 @@ export const authRouter = router({
             where: {
                 email: {
                     equals: email
-                }
+                },
             }
         })
 
@@ -30,6 +32,44 @@ export const authRouter = router({
             }
         })
 
-        return { success: true, sendToEmail: email }
+        return { success: true, sentToEmail: email }
+    }),
+    verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input
+
+      const payload = await getPayloadClient()
+
+      const isVerified = await payload.verifyEmail({
+        collection: 'users',
+        token,
+      })
+
+      if (!isVerified)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+      return { success: true }
+    }),
+    signIn: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input, ctx }) => {
+        const { email, password } = input
+        const { res } = ctx
+
+        const payload = await getPayloadClient();
+
+        try {
+            await payload.login({
+                collection: 'users',
+                data: {
+                    email,
+                    password
+                },
+                res
+            })
+
+            return { success: true }
+        } catch (error) {
+            throw new TRPCError({ code: "UNAUTHORIZED" })
+        }
     })
 })
